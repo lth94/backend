6f3d3c943d35d746c23dda95dd22ae74e5aaf86f
lth94 <lth_94@naver.com>
2022-10-26 14:11:17 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+
+@app.route("/hello")
+def hello():
+    result = {"code": 200, "message": "hello flask"}
+    return result
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
